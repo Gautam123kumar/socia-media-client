e2594c37ddc16aaea2c2e8edd3f5f41729ee0f2f
gautam123kumar <kumargautam9518@gmail.com>
2021-05-09 10:44:36 +0530

    profile

--- src/components/rightbar/Rightbar.js
+++ src/components/rightbar/Rightbar.js
@@ -4,10 +4,10 @@ import "./rightbar.css";
 import {Users} from '../../dummyData'
 import Online from '../online/Online';
 
-export default function rightbar() {
-    return (
-        <div className='rightbar'>
-            <div className='rightbarWrapper'>
+export default function rightbar({profile}) {
+    const HomeRightbar = ()=>{
+        return(
+            <>
                 <div className='birthdayContainer'>
                     <img className='birthdayImg' src='assets/gift.png' alt='' />
                     <span className='birthdayText'>
@@ -22,6 +22,61 @@ export default function rightbar() {
                     ))}
                     
                 </ul>
+            </>
+        )
+    };
+    const ProfileRightbar = () =>{
+        return(
+           <>
+            <h4 className='rightbarTitle'>User Informetion</h4>
+            <div className='rightbarInfo'>
+                <div className='rightbarInfoItem'>
+                    <span className='rightbarInfoKey'>City:</span>
+                    <span className='rightbarInfoValue'>Patna</span>
+                </div>
+                <div className='rightbarInfoItem'>
+                    <span className='rightbarInfoKey'>From:</span>
+                    <span className='rightbarInfoValue'>India</span>
+                </div>
+                <div className='rightbarInfoItem'>
+                    <span className='rightbarInfoKey'>Reletionship:</span>
+                    <span className='rightbarInfoValue'>Single</span>
+                </div>
+            </div>
+            <h4 className='rightbarTitle'>User Informetion</h4>
+            <div className='rightbarFollowings'>
+                <div className='rightbarFollowing'>
+                    <img src='assets/person/1.jpeg' alt='' className='rightbarFollowingImg' />
+                    <span className='rightbarFollowingName'>Gautam Kumar</span>
+                </div>
+                <div className='rightbarFollowing'>
+                    <img src='assets/person/2.jpeg' alt='' className='rightbarFollowingImg' />
+                    <span className='rightbarFollowingName'>Gautam Kumar</span>
+                </div>
+                <div className='rightbarFollowing'>
+                    <img src='assets/person/3.jpeg' alt='' className='rightbarFollowingImg' />
+                    <span className='rightbarFollowingName'>Gautam Kumar</span>
+                </div>
+                <div className='rightbarFollowing'>
+                    <img src='assets/person/4.jpeg' alt='' className='rightbarFollowingImg' />
+                    <span className='rightbarFollowingName'>Gautam Kumar</span>
+                </div>
+                <div className='rightbarFollowing'>
+                    <img src='assets/person/5.jpeg' alt='' className='rightbarFollowingImg' />
+                    <span className='rightbarFollowingName'>Gautam Kumar</span>
+                </div>
+                <div className='rightbarFollowing'>
+                    <img src='assets/person/6.jpeg' alt='' className='rightbarFollowingImg' />
+                    <span className='rightbarFollowingName'>Gautam Kumar</span>
+                </div>
+            </div>
+           </>
+        )
+    }
+    return (
+        <div className='rightbar'>
+            <div className='rightbarWrapper'>
+                <ProfileRightbar />
             </div>
         </div>
     )
